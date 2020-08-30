5b2ed962b3c83f6246865fb99de4171e82ae5bfa
unknown <princekr700@gmail.com>
2020-08-30 18:18:03 +0530

    modified html js and css files and deleted unused images

--- main.js
+++ main.js
@@ -4,13 +4,13 @@
     version - 1.0
 -------------------------------
 =============================*/
-
+/*
 "use strict";
 
 (function ($) {
   /*------------------
 		Navigation
-	--------------------*/
+	--------------------
   $(".nav-switch").on("click", function (event) {
     $(".main-menu").slideToggle(400);
     event.preventDefault();
@@ -18,9 +18,10 @@
 
   /*------------------
 		Background set
-	--------------------*/
+	--------------------
   $(".set-bg").each(function () {
     var bg = $(this).data("setbg");
     $(this).css("background-image", "url(" + bg + ")");
   });
 })(jquery);
+*/

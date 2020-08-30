5066907b42b8a4fc8c6eb3e7b6387eaeb77e5db6
unknown <princekr700@gmail.com>
2020-08-30 17:12:02 +0530

    portfolio 1st commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/* ============================
+-------------------------------
+    kp - Portfolio
+    version - 1.0
+-------------------------------
+=============================*/
+
+"use strict";
+
+(function ($) {
+  /*------------------
+		Navigation
+	--------------------*/
+  $(".nav-switch").on("click", function (event) {
+    $(".main-menu").slideToggle(400);
+    event.preventDefault();
+  });
+
+  /*------------------
+		Background set
+	--------------------*/
+  $(".set-bg").each(function () {
+    var bg = $(this).data("setbg");
+    $(this).css("background-image", "url(" + bg + ")");
+  });
+})(jquery);

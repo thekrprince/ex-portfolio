2848fbe6a3a86f71f556c40aaa47f09993367030
unknown <princekr700@gmail.com>
2020-09-06 22:10:21 +0530

    removed unnecessary code

--- main.js
+++ main.js
@@ -15,17 +15,3 @@ window.addEventListener('load', () => {
 $(window).scroll(function () {
   $('header').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
-
-$('#nav-menu ul li a').click(function () {
-  $('active').removeClass('active');
-  $(this).addClass('active');
-});
-$(window).scroll(function () {
-  var href = $(this).scrollTop();
-  $('.link').each(function (event) {
-    if (href >= $($(this).attr('href')).offset().top - 1) {
-      $('active').removeClass('active');
-      $(this).addClass('active');
-    }
-  });
-});

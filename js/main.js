82ba51e135066cb0cfdcf87ea074eeccf8024f5c
princekr96 <princekr700@gmail.com>
2021-08-25 15:33:10 +0530

    nav bar logic added

--- js/main.js
+++ js/main.js
@@ -11,6 +11,29 @@ window.addEventListener('load', () => {
   preloader.classList.add('preload-finish');
 });
 
+/*------- Nav section -------*/
+var navLinks = document.querySelectorAll('.nav-link');
+var sections = document.querySelectorAll('.section');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach((nav) => {
+    nav.classList.remove('active');
+    if (nav.classList.contains(current)) {
+      nav.classList.add('active');
+    }
+  });
+});
+
 /*------- Nav bar background change when scroll --------*/
 $(window).scroll(function () {
   $('header').toggleClass('scrolled', $(this).scrollTop() > 50);
